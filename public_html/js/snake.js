dc0d5285cb4ff492d392f98836b28aae69b10e9b
Blair Wright <blairbwright@gmail.com>
2014-10-06 15:04:19 -0700

    wwwd

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,3 +1,24 @@
+var snake;
 
+var context;
+var screenWidth;
+var screenHeight;
+
+function gameInitialize() {
+    var canvas = document.getElementbyId("game-screen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height =screenHeight;
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    
+}
 
 
